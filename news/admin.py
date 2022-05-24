e4da01498cbad7e3671b020aa292c503547e80f6
Stefan-ci <claverdiby9@gmail.com>
2022-05-24 20:15:18 +0000

    Customized and cleaned up Article (and proxies also) admins

--- news/admin.py
+++ news/admin.py
@@ -1,47 +1,131 @@
 from news.models import Article
-from django.contrib import admin, messages
-from django.utils.translation import ngettext
+from django.contrib import admin
+from news.utils import ArticleModelAdminStuffs
+from news.models import PremiumArticle, NonPremiumArticle, PrivateArticle
+from news.models import PublicArticle, SubmitedArticle, NonSubmitedArticle
 
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'language', 'website', 'added_on']
+
+
+class ArticleAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'added_on']
     list_filter = ['is_public', 'is_premium', 'is_submited', 'added_on', 'language']
-    actions = ['mark_as_public', 'mark_as_private']
-    search_fields = ['title', 'summary', 'author', 'content', 'tags', 'category', 'published_on',
-        'language', 'base_url', 'website', 'added_on']
-
-    date_hierarchy = 'added_on'
-
-
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    
-    # def has_change_permission(self, request, obj=None):
-    #     return False
     
 
 
-    def mark_as_public(self, request, queryset):
-        updated = queryset.update(is_public=True)
-        self.message_user(request, ngettext(
-                "%d user successfully activated",
-                "%d users successfully activated",
-                updated,
-            ) % updated, messages.SUCCESS)
-    mark_as_public.short_description = 'Mark slected articles as public'
 
 
 
-    def mark_as_private(self, request, queryset):
-        updated = queryset.update(is_public=False)
-        self.message_user(request, ngettext(
-                "%d user successfully activated",
-                "%d users successfully activated",
-                updated,
-            ) % updated, messages.SUCCESS)
-    mark_as_private.short_description = 'Mark slected articles as private'
+"""Below are proxy models admin customizations"""
+
+class PremiumAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_premium', 'added_on']
+    list_filter = ['is_public', 'is_submited', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_premium=True)
+        return qs
+
+
+
+
+
+
+
+
+class NonPremiumAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_premium', 'added_on']
+    list_filter = ['is_public', 'is_submited', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_premium=False)
+        return qs
+
+
+
+
+
+
+
+class PrivateAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_public', 'added_on']
+    list_filter = ['is_premium', 'is_submited', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_public=False)
+        return qs
+
+
+
+
+
+
+
+class PublicAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_public', 'added_on']
+    list_filter = ['is_premium', 'is_submited', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_public=True)
+        return qs
+
+
+
+
+
+
+
+
+class SubmitedAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_submited', 'added_on']
+    list_filter = ['is_public', 'is_premium', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_submited=True)
+        return qs
+
+
+
+
+
+
+
+
+class NonSubmitedAdmin(ArticleModelAdminStuffs):
+    list_display = ['title', 'author', 'is_submited', 'added_on']
+    list_filter = ['is_public', 'is_premium', 'added_on', 'language']
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = Article.objects.filter(is_submited=False)
+        return qs
+
+
+
+
 
 
 
 admin.site.register(Article, ArticleAdmin)
+
+admin.site.register(PremiumArticle, PremiumAdmin)
+admin.site.register(NonPremiumArticle, NonPremiumAdmin)
+
+admin.site.register(PrivateArticle, PrivateAdmin)
+admin.site.register(PublicArticle, PublicAdmin)
+
+admin.site.register(SubmitedArticle, SubmitedAdmin)
+admin.site.register(NonSubmitedArticle, NonSubmitedAdmin)
+
