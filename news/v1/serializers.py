c73777c0ccc9115240e8d994bcdfdf66903d1a69
Stefan-ci <claverdiby9@gmail.com>
2022-05-25 11:13:22 +0000

    Removed unnecessary fields in news' serializers

--- news/v1/serializers.py
+++ news/v1/serializers.py
@@ -7,9 +7,8 @@ class ArticleSerializer(ModelSerializer):
     submited_by = ReadOnlyField(source='submited_by.username')
     class Meta:
         model = Article
-        fields = ['id', 'submited_by', 'title', 'author', 'picture_url', 'content', 'summary', 
-            'article_tags', 'category', 'published_on', 'language', 'website', 'is_premium', 
-            'is_public', 'is_submited', 'added_on']
+        fields = ['title', 'author', 'picture_url', 'content', 'summary', 
+            'article_tags', 'category', 'published_on', 'language', 'website']
 
 
 
